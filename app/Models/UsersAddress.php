e29afd454af84451da44f921e7ca1b0d40d9d1cf
vineetbellway <vineetbellway@gmail.com>
2023-11-02 17:18:43 +0530

    bug fixes in delivery address api

--- app/Models/UsersAddress.php
+++ app/Models/UsersAddress.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Models\Model;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UsersAddress extends Model
 {
+    protected $table = 'users_addresses';
+    
+    protected $fillable = ['house_name','floor_number','landmark','zip_code','address_id','customer_id'];
     use HasFactory;
 }
