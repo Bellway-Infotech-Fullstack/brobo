9a130428071705f0b76b814a56e0473e1d4f30d3
vineetbellway <vineetbellway@gmail.com>
2023-11-08 16:31:04 +0530

    add image in sub ctaegory and resolve issue in product detail and product list api

--- app/Models/UsersAddress.php
+++ app/Models/UsersAddress.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersAddress extends Model
 {
-    protected $table = 'users_addresses';
-    
-    protected $fillable = ['house_name','floor_number','landmark','zip_code','address_id','customer_id'];
+    protected $table = 'users_addresses';    
+        
+    protected $fillable = ['house_name','floor_number','landmark','zip_code','address_id','customer_id','area_name'];
     use HasFactory;
 }
