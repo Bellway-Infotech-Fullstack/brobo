6ada3befce61757e3c1d8fc8f1ab5629d337c808
vineetbellway <vineetbellway@gmail.com>
2024-02-21 12:05:28 +0530

    shipping policy and other new  policy functionality in admin side

--- resources/views/admin-views/business-settings/shipping-policy.blade.php
+++ resources/views/admin-views/business-settings/shipping-policy.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin.app')
 
-@section('title','Privacy Policy')
+@section('title','Shipping Policy')
 
 @push('css_or_js')
 
@@ -12,17 +12,17 @@
         <div class="page-header_ pb-4">
             <div class="row align-items-center">
                 <div class="col-sm mb-2 mb-sm-0">
-                    <h1 class="page-header-title">Privacy Policy</h1>
+                    <h1 class="page-header-title">Shipping Policy</h1>
                 </div>
             </div>
         </div>
         <!-- End Page Header -->
         <div class="row gx-2 gx-lg-3">
             <div class="col-sm-12 col-lg-12 mb-3 mb-lg-2">
-                <form action="{{route('admin.business-settings.privacy-policy')}}" method="post" id="tnc-form">
+                <form action="{{route('admin.business-settings.shipping-policy')}}" method="post" id="tnc-form">
                     @csrf
                     <div class="form-group">
-                        <textarea class="ckeditor form-control" name="privacy_policy">{!! $data['value'] !!}</textarea>
+                        <textarea class="ckeditor form-control" name="shipping_policy">{!! $data['value'] !!}</textarea>
                     </div>
 
                     <button type="submit" class="btn btn-primary">{{__('messages.submit')}}</button>
